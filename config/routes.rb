e6ae7d12a2669667b008f0cc6b338070be52b882
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-07-15 11:33:10 +0530

    Log In functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root to: "main#index"
+
+  get "/about", to: "about#index"
+
   get 'sign_up', to: "registeration#new"
   post 'sign_up', to: "registeration#create"
-  get "/about", to: "about#index"
+
+  get '/log_in', to: "session#new"
+  post '/log_in', to: "session#create"
   delete '/logout', to: "session#destroy"
 end
