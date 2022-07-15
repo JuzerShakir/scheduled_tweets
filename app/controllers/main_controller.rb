f06073f69972c37834c1536d8bf1e84593022f1a
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-07-15 12:48:07 +0530

    added signin/signup links to navbar

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,5 @@
 class MainController < ApplicationController
     def index
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
         # flash.now[:notice] = "Login Successfully"
         # flash.now[:alert] = "Invalid Login"
     end
